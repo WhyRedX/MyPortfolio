61938919264ac8f0da217375d936ed52c803fa40
WhyRedX <thapabhupendra9779@gmail.com>
2022-06-13 23:59:13 +0530

    structured

---
+++ data/projectsCard.js
@@ -0,0 +1,32 @@
+const projectsCard = [
+    {
+      projectName: "calisthenxs",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "adasdasd",
+    },
+    {
+      projectName: "book-kart",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "adasdasd",
+    },
+    {
+      projectName: "dummy name",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "adasdasd",
+    },
+    {
+      projectName: "dummy name",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "adasdasd",
+    },
+  ];
+
+  export default projectsCard
