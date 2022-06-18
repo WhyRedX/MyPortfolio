21430161554afde155be67f4dc741d7283861ad3
WhyRedX <thapabhupendra9779@gmail.com>
2022-06-18 16:23:51 +0530

    additional

---
+++ data/projectsCard.js
@@ -0,0 +1,20 @@
+const projectsCard = [
+    {
+      projectName: "calisthenxs",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "https://github.com/WhyRedX/calisthenxs",
+      demoLink: "https://calisthenxs-production.up.railway.app",
+    },
+    {
+      projectName: "book-kart",
+      language: "html5, css3, javascript, reactjs, nodejs",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+      sourceLink: "https://github.com/WhyRedX/book-kart-app",
+      demoLink: "demo",
+    },
+  ];
+
+  export default projectsCard;
