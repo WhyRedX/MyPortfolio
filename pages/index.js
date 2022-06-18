1d137c43c35770fd9c80372d27f2fd46ce2c562b
WhyRedX <thapabhupendra9779@gmail.com>
2022-06-18 16:12:49 +0530

    component added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
+import squareBlock from "../components/squareBlock";
+
+
 const skillsCard = [
   {
     skillLogo: (
@@ -156,18 +159,6 @@ const projectsCard = [
   },
 ];
 
-function squareBlock({ skillLogo, skillName }) {
-  return (
-    <div className="group">
-      <div className="md:w-full md:h-auto p-4 md:p-5 flex flex-col justify-between shadow-md hover:scale-110 transition group group-hover:bg-xdark">
-        <div>{skillLogo}</div>
-        <span className="text-center text-sm lg:text-lg group-hover:text-xwhite">
-          {skillName}
-        </span>
-      </div>
-    </div>
-  );
-}
 
 function rectangleBlock({
   projectName,
