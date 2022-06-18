4dede37f4817bac4c1d130d53b0fbadc7f728139
WhyRedX <thapabhupendra9779@gmail.com>
2022-06-18 16:49:31 +0530

    description added

--- data/projectsCard.js
+++ data/projectsCard.js
@@ -3,7 +3,7 @@ const projectsCard = [
       projectName: "calisthenxs",
       language: "html5, css3, javascript, reactjs, nodejs",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+        "a gym site (frontend project) developed using reactjs only. it is only a user-interface part.",
       sourceLink: "https://github.com/WhyRedX/calisthenxs",
       demoLink: "https://calisthenxs-production.up.railway.app",
     },
@@ -11,7 +11,7 @@ const projectsCard = [
       projectName: "book-kart",
       language: "html5, css3, javascript, reactjs, nodejs",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste saepe rerum minus voluptate. Velit nulla nisi harum, rerum in ipsum?",
+        "a online book store site developed using reactjs and mongodb. (under development)",
       sourceLink: "https://github.com/WhyRedX/book-kart-app",
       demoLink: "demo",
     },
