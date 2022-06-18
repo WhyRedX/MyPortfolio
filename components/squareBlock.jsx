7bebd42a99812dc09b8e82c18f6ab6283871354d
WhyRedX <thapabhupendra9779@gmail.com>
2022-06-18 16:15:19 +0530

    updated rectangle

--- components/squareBlock.jsx
+++ components/squareBlock.jsx
@@ -1,12 +1,12 @@
 export default function squareBlock({ skillLogo, skillName }) {
-    return (
-      <div className="group">
-        <div className="md:w-full md:h-auto p-4 md:p-5 flex flex-col justify-between shadow-md hover:scale-110 transition group group-hover:bg-xdark">
-          <div>{skillLogo}</div>
-          <span className="text-center text-sm lg:text-lg group-hover:text-xwhite">
-            {skillName}
-          </span>
-        </div>
+  return (
+    <div className="group">
+      <div className="md:w-full md:h-auto p-4 md:p-5 flex flex-col justify-between shadow-md hover:scale-110 transition group group-hover:bg-xdark">
+        <div>{skillLogo}</div>
+        <span className="text-center text-sm lg:text-lg group-hover:text-xwhite">
+          {skillName}
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import squareBlock from "../components/squareBlock";
+import rectangleBlock from "../components/rectangleBlock";
 
 
 const skillsCard = [
@@ -160,51 +161,7 @@ const projectsCard = [
 ];
 
 
-function rectangleBlock({
-  projectName,
-  language,
-  description,
-  sourceLink,
-  demoLink,
-}) {
-  return (
-    <div className="group">
-      <div className="md:w-full md:h-auto p-5 shadow-md hover:scale-110 group-hover:bg-xdark transition">
-        <h1 className="font-bold text-2xl mb-1 group-hover:text-xwhite">
-          {projectName}
-        </h1>
-        <span className="text-xsmall group-hover:text-xwhite">
-          [ <em>{language}</em> ]
-        </span>
-        <p className="text-xsmall my-3.5 group-hover:text-xwhite">
-          {description}
-        </p>
-        <div className="flex items-center">
-          <a
-            className="text-xsmall bg-black group-hover:bg-xwhite py-1.5 px-2 mr-2.5"
-            href={sourceLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className=" text-xwhite group-hover:text-xdark">
-              source code
-            </span>
-          </a>
-          <a
-            className="text-xsmall bg-black group-hover:bg-xwhite py-1.5 px-2"
-            href={demoLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="text-xwhite group-hover:text-xdark">
-              Live demo
-            </span>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
+
 
 export default function Home() {
   return (
